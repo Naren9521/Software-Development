90111503a0c7a10073478ceb9043ffe56f6e5434
sahil kumar <kumarsahil7753@gmail.com>
2024-10-03 18:39:24 +0530

    adding contact page

--- GramAi/src/components/Header.jsx
+++ GramAi/src/components/Header.jsx
@@ -1,3 +1,6 @@
+import React from 'react';
+import { Link } from 'react-router-dom';  // Import Link for routing
+
 const Header = () => {
   return (
     <header className="header">
@@ -5,11 +8,11 @@ const Header = () => {
         <i className="fas fa-broadcast-tower"></i> GramAi
       </div>
       <nav>
-        <a href="#">Home</a>
-        <a href="#">About</a>
-        <a href="#">Features</a>
-        <a href="#">Contact</a>
-        <a className="join-now" href="/dashboard">Join Now</a>
+        <Link to="/">Home</Link>          {/* Use Link instead of a */}
+        <Link to="/about">About</Link>    {/* Link to About */}
+        <Link to="/features">Features</Link>  {/* Link to Features */}
+        <Link to="/contact">Contact</Link>    {/* Link to Contact */}
+        <Link className="join-now" to="/dashboard">Join Now</Link> {/* Link to dashboard */}
       </nav>
     </header>
   );
