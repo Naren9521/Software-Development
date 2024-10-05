dac68dc8330ac0f83d67680427898a1b038d9e77
naren9521 <ns952140@gmail.com>
2024-10-05 23:21:45 +0530

    fixed auth page

--- GramAi/src/App.jsx
+++ GramAi/src/App.jsx
@@ -1,13 +1,18 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LandingPage from './pages/LandingPage';
 import DashboardPage from './pages/DashboardPage';
-
+import ToolsPage from './pages/ToolsPage';
+import Contact from './components/Contact';
+import AuthPage from './pages/AuthPage';
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/dashboard" element={<DashboardPage />} />
+        <Route path="/InteractiveTools" element={<ToolsPage />} />
+        <Route path="/contact" element={<Contact />} /> 
+        <Route path = "/login" element ={<AuthPage />} />
       </Routes>
     </Router>
   );
